15d4866ec2ddef18de0f120de8b7d511505c425a
Andrey Vlasov <troller98@mail.ru>
2023-03-11 16:48:50 +0300

    добавили функцию заглавных, решили проблемсы

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def users():
+    '''docstring'''
     user_inputs = str.upper(input())
     return user_inputs
+
+def users2():
+    '''docstrings'''
+    user_inpu = str.title(input())
+    return user_inpu
